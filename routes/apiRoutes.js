003b42aeea00c4083efed88428f3eee65fe45421
jjalanis <jjalanis@gmail.com>
2018-07-28 12:59:26 -0500

    carrier logic

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,20 @@
 var db = require("../models");
 
+
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // Api route to get all guides and its info. 
+  app.get("/api/tracks", function(req, res) {
+    db.showTracks.findAll({}).then(function(dbTracks) {
+      res.json(dbTracks);
     });
   });
 };
+
+//Route to write new track and it current status
+app.post("/api/authors", function(req, res) {
+  
+  db.Author.create(req.body).then(function(dbAuthor) {
+    res.json(dbAuthor);
+  });
+});
