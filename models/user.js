1ac73efc85c3ae4975f3e77567df5b0859c4d59f
laurabeltran03 <37489845+laurabeltran03@users.noreply.github.com>
2018-07-28 13:13:58 -0500

    update User

--- models/user.js
+++ models/user.js
@@ -1,7 +1,30 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var Users = sequelize.define("Users", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    EmailAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    passwordUser: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false
+    }
   });
-  return Example;
+
+
+
+  return Users;
 };
