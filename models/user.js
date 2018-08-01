b0fed55a380197194372691ef85438a55d7dca38
marioreg <marioreg@hotmail.com>
2018-07-31 19:49:00 -0500

    aded login functionality

--- models/user.js
+++ models/user.js
@@ -1,30 +1,17 @@
-module.exports = function(sequelize, DataTypes) {
-  var Users = sequelize.define("Users", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    EmailAddress: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    passwordUser: {
-      type: DataTypes.STRING.BINARY,
-      allowNull: false
-    }
-  });
 
+module.exports = function(sequelize, Sequelize) {
 
+	var User = sequelize.define('user', {
+		id: { autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER},
+		firstname: { type: Sequelize.STRING,notEmpty: true},
+		lastname: { type: Sequelize.STRING,notEmpty: true},
+		email: { type:Sequelize.STRING, validate: {isEmail:true} },
+		password : {type: Sequelize.STRING,allowNull: false },
+		last_login: {type: Sequelize.DATE},
+        status: {type: Sequelize.ENUM('active','inactive'),defaultValue:'active' }
 
-  return Users;
-};
+});
+
+	return User;
+
+}
