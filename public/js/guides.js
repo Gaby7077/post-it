da594a3515c97a1036543c16c9ef6123b0c53416
Gaby7077 <gamma@gmail.com>
2018-08-02 17:50:15 -0500

    cambios 3

--- public/js/guides.js
+++ public/js/guides.js
@@ -2,22 +2,12 @@ $(document).ready(function () {
 
     var carrierInput = $("#carrier");
     var trackingNum = $("#tracking");
+    var guidesList = $("tbody");
+    var guidesContainer = $(".guides-container");
+  
 
-    $(document).on("submit", "#new-guide", handleAuthorFormSubmit);
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $(document).on("submit", "#author-form", handleFormSubmit);
+  $(document).on("click", ".delete-author", handleDeleteButtonPress);
 
 
 
