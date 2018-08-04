cb274e3b41b449b8b5c884df26f173288ecac525
Mario Reyes <33184843+marioreg@users.noreply.github.com>
2018-08-04 02:21:08 -0500

    Actualizacion ruta tracking

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,27 @@
 var authController = require('../controllers/authcontroller.js');
+var express    = require('express');
+var app        = express();
+var path = require("path");
+
+
 
 module.exports = function(app,passport){
+    app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/signup', authController.signup);
 
 app.get('/signin', authController.signin);
 
 app.post('/signup', passport.authenticate('local-signup',
-{ successRedirect: '/dashboard', failureRedirect: '/signup'}
+{ successRedirect: '/tracking', failureRedirect: '/signup'}
 ));
 
-app.get('/dashboard',isLoggedIn, authController.dashboard);
+//app.get('/dashboard',isLoggedIn, authController.dashboard);
+app.get('/tracking',isLoggedIn, authController.tracking);
+
 app.get('/logout',authController.logout);
 app.post('/signin', passport.authenticate('local-signin',
-{ successRedirect: '/dashboard', failureRedirect: '/signin'}
+{ successRedirect: '/tracking', failureRedirect: '/signin'}
 ));
 
 function isLoggedIn(req, res, next) {
@@ -21,9 +29,6 @@ function isLoggedIn(req, res, next) {
         return next();
     res.redirect('/signin');
 }};
-
-
-
 
 
 
